7a2e3fbc3136448c5a4c21ad1e91d7af17f7b30b
Druhin Bala <druhin.bala@gmail.com>
2020-02-13 21:59:45 -0500

    signup page layout

--- navigation/lib/login_page.dart
+++ navigation/lib/login_page.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  void _login(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Worlder")),
-        body: Center(child: Text("Login page")));
+    return Container(
+        color: Colors.white,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Create your account",
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.white,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(227, 227, 227, 1),
+                          hintText: 'First Name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(227, 227, 227, 1),
+                          hintText: 'Last Name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(227, 227, 227, 1),
+                          hintText: 'Phone',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(227, 227, 227, 1),
+                          hintText: 'Email',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(227, 227, 227, 1),
+                          hintText: 'Password',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: 155,
+                        height: 48,
+                        child: RaisedButton(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          elevation: 3,
+                          color: Colors.lightBlue,
+                          onPressed: () {
+                            _login(context);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: Text(
+                        "By signing up, you agree with the Terms of Service & Privacy Policy",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
